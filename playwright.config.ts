899602f3ba24e535c60243c19455a7cddcf2472b
Nathan Huynh <natang2108@icloud.com>
2026-04-29 02:40:02 +0100

    fix: resolve playwright.config.ts merge conflict

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,18 +26,12 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-<<<<<<< HEAD
-    // baseURL: 'http://localhost:3000',
-=======
     baseURL: 'http://localhost:3000',
->>>>>>> main
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
 
-<<<<<<< HEAD
-=======
   webServer: {
     command: "npm run dev",
     url: "http://localhost:3000",
@@ -45,7 +39,6 @@ export default defineConfig({
     timeout: 120 * 1000,
   },
 
->>>>>>> main
   /* Configure projects for major browsers */
   projects: [
     {
@@ -83,11 +76,4 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
